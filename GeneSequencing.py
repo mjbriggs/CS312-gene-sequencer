2a9bcec61736532eb461a3a1105bbd06e553d1d9
Michael Briggs <mjbriggs@icloud.com>
2019-11-06 17:34:11 -0700

    unbounded working

--- GeneSequencing.py
+++ GeneSequencing.py
@@ -29,24 +29,69 @@ class GeneSequencing:
 		self.banded = banded
 		self.MaxCharactersToAlign = align_length
 		results = []
+		
 
-		self.sequencer = UnbandedSequencer(sequences[0], sequences[1])
-		self.sequencer.fill()
+		# sequencer = UnbandedSequencer(sequences[0], sequences[1])
+		# print(sequencer.seq1)
+		# print(sequencer.seq2)
+		# sequencer.fill()
+		# sequencer.printTable()
+		# row = len(sequencer.table) - 1
+		# col = len(sequencer.table[0]) - 1
+		# sequencer.printTable()
+		# sequencer.build(row, col)
+		# alignment1 = sequencer.iString
+		# alignment2 = sequencer.jString
+		# print(alignment1)
+		# print(alignment2)
+		# score = sequencer.score
+		# print(score)
+		# sequencer.reverseStrings()
 		
 		for i in range(len(sequences)):
+		# for i in range(10):
 			jresults = []
 			for j in range(len(sequences)):
+			# for j in range(6):
 
 				if(j < i):
 					s = {}
 				else:
+					# if i == 2 and j == 7:
+					# 	s = {'align_cost':69, 'seqi_first100':sequences[i][:100], 'seqj_first100':sequences[i][:100]}
+					# 	table.item(i,j).setText('{}'.format(int(69) if 69 != math.inf else 69))
+					# 	table.update()	
+					# else:
+					print("at ", i , " and ", j)
 ###################################################################################################
 # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
-					score = i+j;
-					alignment1 = 'abc-easy  DEBUG:(seq{}, {} chars,align_len={}{})'.format(i+1,
-						len(sequences[i]), align_length, ',BANDED' if banded else '')
-					alignment2 = 'as-123--  DEBUG:(seq{}, {} chars,align_len={}{})'.format(j+1,
-						len(sequences[j]), align_length, ',BANDED' if banded else '')
+					sequencer = UnbandedSequencer(sequences[i][:align_length], sequences[j][:align_length])
+					# print(sequencer.seq1)
+					# print(sequencer.seq2)
+					print("filling")
+					sequencer.fill()
+					print("done filling")
+					# sequencer.printTable()
+					row = len(sequencer.table) - 1
+					col = len(sequencer.table[0]) - 1
+					# sequencer.printTable()
+					print("building at ", row, " , ", col)
+					sequencer.build(row, col)
+					print("done building")
+					alignment1 = sequencer.iString
+					if(len(alignment1) > 100):
+						alignment1 = alignment1[:100]
+					alignment2 = sequencer.jString
+					if(len(alignment2) > 100):
+						alignment2 = alignment2[:100]
+					print("alignment1 ", alignment1)
+					print("alignment2 ", alignment2)
+					score = sequencer.score
+					print("score ", score)
+					# alignment1 = 'abc-easy  DEBUG:(seq{}, {} chars,align_len={}{})'.format(i+1,
+					# 	len(sequences[i]), align_length, ',BANDED' if banded else '')
+					# alignment2 = 'as-123--  DEBUG:(seq{}, {} chars,align_len={}{})'.format(j+1,
+					# 	len(sequences[j]), align_length, ',BANDED' if banded else '')
 ###################################################################################################					
 					s = {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
 					table.item(i,j).setText('{}'.format(int(score) if score != math.inf else score))
--- UnbandedSequencer.py
+++ UnbandedSequencer.py
@@ -6,6 +6,7 @@ class UnbandedSequencer:
     #seq1 goes across the top 
     #seq2 goes down the side
     def __init__(self, seqa, seqb):
+        # print("__init__")
         if len(seqa) > len(seqb):
             self.seq1 = seqa
             self.seq2 = seqb
@@ -13,23 +14,51 @@ class UnbandedSequencer:
             self.seq1 = seqb
             self.seq2 = seqa
 
+        self.endRow = 0
+        self.endCol = 0
+
+        self.iString = self.seq1
+        self.jString = self.seq2
+        # self.seq1 = "-" + self.seq1
+        # self.seq2 = "-" + self.seq2
+        
+        self.score = 0
+
         self.currentDirection = LEFT
-        self.table = [[0 for i in range(len(self.seq1))] for j in range(len(self.seq2))] 
-        self.directions = [[0 for i in range(len(self.seq1))] for j in range(len(self.seq2))] 
+        # # print("building tables")
+        self.table = [[0]]
+        # self.table = [[0 for i in range(len(self.seq1))] for j in range(len(self.seq2))] 
+        # # print("built weight table")
+        # self.directions = [[0 for i in range(len(self.seq1))] for j in range(len(self.seq2))] 
+        self.directions = [[]]
+        # # print("built dir table")
         self.initTable()
 
+    def reverseStrings(self):
+        self.iString = self.iString[::-1]
+        self.jString = self.jString[::-1]
+        # # print(self.iString)
+        # # print(self.jString)
+
     def initTable(self):
+        # print("initTable")
+        for i in range(len(self.seq1)):
+            self.table[0].append(INDEL * (i+1))
+            self.directions[0].append(None)
+            self.endCol += 1
 
-        for i in range(len(self.table)):
-            self.table[i][0] = INDEL * i
+        # self.# printTable()
+        for j in range(len(self.seq2)):
+            self.table.append([INDEL * (j + 1)])
+            self.directions.append([None])
+            self.endRow += 1
 
-        for j in range(len(self.table[0])):
-            self.table[0][j] = INDEL * j
         
-        self.printTable()
+        #self.printTable()
 
 
     def diff(self, rowJ, colI):
+        # print("in diff row is ", rowJ, " col is ", colI)
         if self.seq1[colI] == self.seq2[rowJ]:
             return MATCH
         else:
@@ -50,26 +79,110 @@ class UnbandedSequencer:
         return self.table[rowJ][colI]
 
     def currentE(self, rowJ, colI):
-        diagonal = self.diff(rowJ, colI) + self.existingE(rowJ - 1, colI - 1)
+        stringJIndex = rowJ - 1 
+        stringIIndex = colI - 1
+        # print("rowj is ", rowJ, " colI is ", colI)
+        diagonal = self.diff(stringJIndex, stringIIndex) + self.existingE(rowJ - 1, colI - 1)
         left = INDEL + self.existingE(rowJ, colI - 1)
         top = INDEL + self.existingE(rowJ - 1, colI)
 
         return self.minimum(left, top, diagonal)
 
     def fill(self):
-        for j in range(len(self.table)):
-            if j > 0:
-                for i in range(len(self.table[0])):
-                    if i > 0:
-                        self.table[j][i] = self.currentE(j, i)
-                        self.directions[j][i] = self.currentDirection
+        # print("filling table")
+        # print("num rows ", len(self.table))
+        # print("num cols ", len(self.table[0]))
+        j = 1 
+        while j < (len(self.table)):
+            i = 1
+            while i < (len(self.table[0])): 
+                # print("j is ", j, " and i is ", i)
+                self.table[j].append(self.currentE(j, i))
+                self.directions[j].append(self.currentDirection)
+                i += 1 
+            j += 1
         
-        self.printTable()
+        #self.printTable()
+    def setScore(self, rowJ, colI):
+        self.score = self.table[rowJ][colI]
+
+    def betterBuild(self, rowJ, colI):
+        direction = self.directions[rowJ][colI]
+        if direction == TOP:
+            # self.iString += "-"
+            # self.jString += self.seq2[rowJ]
+            rowJ -= 1
+            self.iString = self.iString[:colI] + "-" + self.iString[colI:]
+            # self.build(rowJ - 1, colI)
+        elif direction == LEFT:
+            # self.iString += self.seq1[colI]
+            # self.jString += "-"
+            self.jString = self.jString[:rowJ] + "-" + self.jString[rowJ:]
+            # self.build(rowJ, colI - 1)
+            colI -= 1
+        elif direction == DIAGONAL:
+            # self.iString += self.seq1[colI]
+            # self.jString += self.seq2[rowJ]
+            # self.build(rowJ - 1, colI - 1)
+            rowJ -= 1
+            colI -= 1
+        else:
+            return
+
+    def build(self, rowJ, colI):
+        # print("row ", rowJ, " col ", colI)
+        # print("rowlen ", len(self.table), ", col len", len(self.table[0]))
+        self.score = self.table[rowJ][colI]
+        while rowJ >= 0:
+            while colI >= 0:
+                direction = self.directions[rowJ][colI]
+                if direction == None:
+                    if rowJ > 0:
+                        rowJ -= 1
+                    if colI > 0:
+                        colI -= 1
+                    print("None value at row ", rowJ, ", colI ", colI)
+
+                # # print(self.iString)
+                # # print(self.jString)
+
+                if direction == TOP:
+                    # self.iString += "-"
+                    # self.jString += self.seq2[rowJ]
+                    rowJ -= 1
+                    self.iString = self.iString[:colI] + "-" + self.iString[colI:]
+                    # self.build(rowJ - 1, colI)
+                elif direction == LEFT:
+                    # self.iString += self.seq1[colI]
+                    # self.jString += "-"
+                    self.jString = self.jString[:rowJ] + "-" + self.jString[rowJ:]
+                    # self.build(rowJ, colI - 1)
+                    colI -= 1
+                else:
+                    # self.iString += self.seq1[colI]
+                    # self.jString += self.seq2[rowJ]
+                    # self.build(rowJ - 1, colI - 1)
+                    rowJ -= 1
+                    colI -= 1
+                
+                # if rowJ < 0 and colI >= 0:
+                #     rowJ = 0
+                # elif rowJ >= 0 and colI < 0:
+                #     colI = 0
+
+
+
 
     def printTable(self):
-        print("weights")
+        # print("weights")
+        # print(self.seq1)
+        i = 0
         for row in self.table:
-            print(row)
+            if(i == 0):
+                print("- " , row)
+            else:
+                print(self.seq2[i - 1] , " " , row)
+            i += 1
         
         print("directions")
         for row in self.directions:
@@ -101,4 +214,3 @@ class UnbandedSequencer:
     #
     #
     #
-
